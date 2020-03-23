42fb3dcf2025082118c902dafaa67475a5b212f8
DavidLineman <dline215@gmail.com>
2020-03-23 15:41:17 -0400

    added redirect to destroy action

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -30,7 +30,7 @@ class EventsController < ApplicationController
   def destroy
     @event = Event.find(params[:id])
     @event.destroy
-    redirect_to events_path
+    redirect_to events_url
   end
 
   private 
