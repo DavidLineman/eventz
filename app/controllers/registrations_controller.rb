22b50e5e41e4b24ff843989972d6524587380696
DavidLineman <dline215@gmail.com>
2020-04-22 15:02:03 -0400

    fixed bug on registration form

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,18 +1,16 @@
 class RegistrationsController < ApplicationController
+  before_action :set_event
 
   def index
-    @event = Event.find(params[:event_id])
     @registrations = @event.registrations
   end
 
   def new
-    @event = Event.find(params[:event_id])
     @registration = @event.registrations.new
   end
 
   def create
-    @event = Event.find(params[:event_id])
-    @registration = @event.registrations.new(params[:registration])
+    @registration = @event.registrations.new(registration_params)
 
     if @registration.save
       redirect_to event_registrations_url(@event), 
@@ -21,5 +19,16 @@ class RegistrationsController < ApplicationController
       render :new
     end
   end
+
+
+  private
+
+    def registration_params
+      params.require(:registration).permit(:name, :email, :how_heard)
+    end
+
+    def set_event
+      @event = Event.find(params[:event_id])
+    end
 
 end
