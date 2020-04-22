6931a5713bc17a64caaea3f4875dcd6d89bd62b1
DavidLineman <dline215@gmail.com>
2020-04-22 14:27:03 -0400

    added registration form

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,4 +4,22 @@ class RegistrationsController < ApplicationController
     @event = Event.find(params[:event_id])
     @registrations = @event.registrations
   end
+
+  def new
+    @event = Event.find(params[:event_id])
+    @registration = @event.registrations.new
+  end
+
+  def create
+    @event = Event.find(params[:event_id])
+    @registration = @event.registration.new(params[:registration])
+
+    if @registration.save
+      redirect_to event_registrations_url(@event), 
+        notice: "Thanks for registering!"
+    else  
+      render :new
+    end
+  end
+
 end
