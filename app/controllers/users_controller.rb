4cb9054fb6f59264ea8e09ab9453165edc82653d
DavidLineman <dline215@gmail.com>
2020-05-08 15:27:08 -0400

    added a few methods to make navigating sinmpler

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   before_action :require_signin, except: [:new, :create]
+  before_action :require_correct_user, only: [:edit, :update, :destroy]
+
 
   def index
     @users = User.all
@@ -24,11 +26,9 @@ class UsersController < ApplicationController
   end
 
   def edit 
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       redirect_to @user, notice: "Your account has successfully been updated!"
     else 
@@ -37,7 +37,6 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    @user = User.find(params[:id])
     @user.destroy
     session[:user_id] = nil
     redirect_to events_url, alert: "Your account has been deleted."
@@ -47,6 +46,11 @@ class UsersController < ApplicationController
 
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+    def require_correct_user
+      user = User.find(params[:id])
+      redirect_to events_url unless current_user?(@user)
     end
 
 end
