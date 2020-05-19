f3ff95c368fe9f8fee50e80ecaad48fffc29fa0d
DavidLineman <dline215@gmail.com>
2020-05-19 14:08:47 -0400

    added the ability to add categories to events and updsated the form

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -50,7 +50,7 @@ class EventsController < ApplicationController
   private 
 
   def event_params
-    params.require(:event).permit(:name, :description, :location, :price, :starts_at, :capacity, :image_file_name)
+    params.require(:event).permit(:name, :description, :location, :price, :starts_at, :capacity, :image_file_name, category_ids: [])
   end
 
 end
